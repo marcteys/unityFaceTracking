f79f5c04a4561ff4f16165f472ac0d45bb988443
Marc Teyssier <marc.teys@gmail.com>
2013-10-08 19:33:06 +0100

    Update movesScript.js

--- Assets/Scripts/movesScript.js
+++ Assets/Scripts/movesScript.js
@@ -1,52 +1,37 @@
 
 
+//Public vars
+
+public var PositionSpeed : float=0.001;
+public var RotationSpeed : float= 5;
+public var transformInput :GameObject;
+
 
 private var csTracker : UDPReceive ;
-
 private var verifOnce : boolean  = true;
-	public var PositionSpeed : float=0.001;
 
-		public var RotationSpeed : float= 5;
-		public var transformInput :GameObject ;
-var Target :Transform;
-   private var oriYaw : float;
-	private var oriPitch: float;
-	private var oriRoll: float;
-
-    private var oriX: float;;
-	private var oriY: float;
-	private var oriZ: float;;
-
-	private var oriRawYaw: float;
-	private var oriRawPitch: float;
-	private var oriRawRoll: float;
-    private var oriRawX: float;;
-	private var oriRawY: float;
-	private var oriRawZ: float;
+	var Target :Transform;
 	
-	private var startPos : Vector3 = Vector3.zero;
+private var oriYaw : float;
+private var oriPitch: float;
+private var oriRoll: float;
 
+private var oriX: float;;
+private var oriY: float;
+private var oriZ: float;;
 
-    public var X: float;
-	public var Y: float;
-	public var Z: float;
-
-
-/*
-    public var Yaw : float;
-	public var Pitch: float;
-	public var Roll: float;
-    public var X: float;;
-	public var Y: float;
-	public var Z: float;;
+private var oriRawYaw: float;
+private var oriRawPitch: float;
+private var oriRawRoll: float;
+private var oriRawX: float;;
+private var oriRawY: float;
+private var oriRawZ: float;
 	
-	public var RawYaw: float;
-	public var RawPitch: float;
-	public var RawRoll: float;
-    public var RawX: float;;
-	public var RawY: float;
-	public var RawZ: float;
-*/
+private var startPos : Vector3 = Vector3.zero;
+
+public var X: float;
+public var Y: float;
+public var Z: float;
 
 function Start () {
 	csTracker = transformInput.GetComponent("UDPReceive");
@@ -54,11 +39,12 @@ function Start () {
 }
 
 
+
 function Update () {
 
-//Debug.Log(csTracker.z.GetType().ToString());
 
 /* RaZ */
+
 	if(verifOnce && csTracker.yaw !=0 )  {
 		initVariables();
 		verifOnce = false;
@@ -66,44 +52,21 @@ function Update () {
 	} if (Input.GetKey ("up")) initVariables();
 	
 	
+	
 /* Update values */
 
 	X = csTracker.xPos - oriX  ;
 	
 	
-	if(Target)
-		{
+	if(Target) {
 		
 		Target.position =startPos + new Vector3(csTracker.xPos*PositionSpeed,csTracker.yPos*PositionSpeed,-csTracker.zPos*PositionSpeed);
 		
 		Target.rotation = Quaternion.Euler(-csTracker.pitch*RotationSpeed,-csTracker.yaw*RotationSpeed,csTracker.roll*RotationSpeed);
-		}
-	else return;
-	
-	
-	
-	
+	}else return;
+
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -118,13 +81,4 @@ function initVariables() {
 	 oriY= csTracker.yPos;
 	oriZ= csTracker.zPos;
 		
-	/*
-	 oriRawYaw= csTracker.RawYaw;
-	 oriRawPitch= csTracker.RawPitch;
-	 oriRawRoll= csTracker.roll;
-    oriRawX= csTracker.RawX;
-	 oriRawY= csTracker.RawY;
-	oriRawZ= csTracker.RawZ;
-*/
-
 }
