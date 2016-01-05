6c0a268db52874a969b390386a85fa3b0e7f8f23
Marc Teyssier <marc.teys@gmail.com>
2016-01-05 19:10:43 +0100

    upgrade(Core): From UnityJS to c#

---
+++ Assets/Scripts/FaceTrackMovment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceTrackMovment : MonoBehaviour {
+
+    [SerializeField]
+    UDPReceive receiver = null;
+
+    Transform startPos = null;
+
+    [SerializeField]
+    float reductionPositionFactor = 1;
+
+    [SerializeField]
+    float reductionRotationFactor = 1;
+
+
+    void Start ()
+    {
+        startPos = transform;
+	}
+	
+	void Update ()
+    {
+        transform.position = new Vector3(receiver.xPos * reductionPositionFactor, receiver.yPos * reductionPositionFactor, -receiver.zPos * reductionPositionFactor );
+        transform.rotation = Quaternion.Euler(-receiver.pitch * reductionRotationFactor, -receiver.yaw * reductionRotationFactor, receiver.roll * reductionRotationFactor);
+    }
+}
